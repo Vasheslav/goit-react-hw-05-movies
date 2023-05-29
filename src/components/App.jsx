b1067341e7b5e27f3c108a692f18addb2e06c4b1
Viasheslav Rashnik <vasheslav1000@gmail.com>
2023-05-29 19:27:20 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 const Home = lazy(() => import('../page/Home'));
 const Movies = lazy(() => import('../page/Movies'));
@@ -17,7 +17,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
